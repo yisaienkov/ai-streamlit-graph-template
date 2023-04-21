b81bf47aa9018ed77ccd64ccecb01b8be89c168f
yisaienkov <yisaienkov@gmail.com>
2023-04-21 16:07:14 +0300

    Add select n nodes, edge view

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import string
+
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -8,12 +10,11 @@ from pyvis.network import Network
 
 def simple_func(df): 
     nx_graph = nx.DiGraph()
-    nx_graph.add_node("x", size=20)
-    nx_graph.add_node("y", size=20)
-    nx_graph.add_node("z", size=20)
-
-    for a in "xyz":
-        for b in "xyz":
+    for x in df.columns:
+        nx_graph.add_node(x, size=20)
+    
+    for a in df.index:
+        for b in df.columns:
             if df.loc[a, b]:
                 nx_graph.add_edge(a, b)
 
@@ -22,8 +23,20 @@ def simple_func(df):
 
     nt.show('test.html', notebook=False)
 
-df = pd.DataFrame(np.zeros((3, 3)), columns=["x", "y", "z"], index=["x", "y", "z"])
+n_nodes = st.sidebar.number_input("N nodes", min_value=1, value=3, max_value=10)
+node_names = list(string.ascii_lowercase[:n_nodes])
+
+df = pd.DataFrame(np.zeros((n_nodes, n_nodes)), columns=node_names, index=node_names)
 df = st.sidebar.experimental_data_editor(df)
+
+nodes = []
+for a in df.index:
+    for b in df.columns:
+        if df.loc[a, b]:
+            nodes.append((a, b))
+
+st.sidebar.text_area("Edge view:", nodes)
+
 simple_func(df)
 
 with open("test.html", 'r', encoding='utf-8') as file:
